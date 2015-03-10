921635cce90f5cfa31edf3b439a0890ea7166df7
Brian Amadio <btamadio@users.noreply.github.com>
2015-03-10 18:44:07 +0100

    Added normalization

--- timeCalc.py
+++ timeCalc.py
@@ -8,13 +8,11 @@
 #Centered gaussian bunches, whose x and y components fully factorize
 #Beam crossing angle = 0
 #Centers of bunches coincide at z=0,t=0
-
-#TODO: Normalization (integrate over z,t)
+#Both 1D and 2D functions are normalized over all z,t
 
 #Example usage:
 #a = timeCalc(0.01,0.05)
 #a.get1D(0.01).Draw()
-
 
 import os,sys
 from ROOT import *
@@ -31,11 +29,11 @@ class timeCalc:
         self._tMin = self._zMin/self._c
         self._tMax = self._zMax/self._c
         self._func2 = TF2("func","[0]*exp((-(x-299792458*y)^2*[2]^2-(x+299792458*y)^2*[1]^2)/(2*[1]^2*[2]^2))",self._zMin,self._zMax,self._tMin,self._tMax)
-        self._func2.SetParameter(0,1)
+        self._func2.SetParameter(0,self._c/(pi*sigmaZ1*sigmaZ2))
         self._func2.SetParameter(1,sigmaZ1)
         self._func2.SetParameter(2,sigmaZ2)
         self._func1 = TF1("func","[0]*exp((-([3]-299792458*x)^2*[2]^2-([3]+299792458*x)^2*[1]^2)/(2*[1]^2*[2]^2))",self._tMin,self._tMax)
-        self._func1.SetParameter(0,1)
+        self._func1.SetParameter(0,self._c/(pi*sigmaZ1*sigmaZ2))
         self._func1.SetParameter(1,sigmaZ1)
         self._func1.SetParameter(2,sigmaZ2)
         self._func1.SetParameter(3,0)
